32a52d80eee47b9e604e070d07ef8a1a9b0e1e2a
Abenezer Amare <wakjirabenezer@gmail.com>
2022-04-11 15:02:25 -0700

    sdf

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,24 +6,15 @@
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-
 {
-char lower_case;
-char upper_case;
+int c;
 
-for (upper_case = 'A'; upper_case <= 'z' upper_case++)
-{
-putchar(upper_case);
-}
-
-for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-{
-putchar(lower_case);
-}
-
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
 
 putchar('\n');
 
 return (0);
-
 }
